ca000a461ce1b9b49729ec0bddaf7ac70bbce870
orianachv <chacon.oriana.19@gmail.com>
2018-05-22 19:57:28 -0300

    hasta redirect add

--- models/index.js
+++ models/index.js
@@ -1,43 +1,67 @@
 var Sequelize = require('sequelize');
 var db = new Sequelize('postgres://localhost:5432/wikistack', {
-    logging: false
+  logging: false,
 });
 
-var Page = db.define('page', {
+var Page = db.define(
+  'page',
+  {
     title: {
-        type: Sequelize.STRING,
-        allowNull: false
+      type: Sequelize.STRING,
+      allowNull: false,
     },
     urlTitle: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        get() {
-            return `/wiki/${this.getDataValue('urlTitle')}`
-        },
+      type: Sequelize.STRING,
+      allowNull: false,
+      get() {
+        return `/wiki/${this.getDataValue('urlTitle')}`;
+      },
     },
-    
+
     content: {
-        type: Sequelize.TEXT,
-        allowNull: false
+      type: Sequelize.TEXT,
+      allowNull: false,
     },
     status: {
-        type: Sequelize.ENUM('open', 'closed')
-    }
-});
-var User = db.define('user', {
-    name: {
-        type: Sequelize.STRING,
-        allowNull: false
+      type: Sequelize.ENUM('open', 'closed'),
     },
-    email: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        validate: {
-            isEmail: true
-        }
-    }
+  },
+  {
+    hooks: {
+      beforeValidate: (page, options) => {
+        page.urlTitle = generateUrlTitle();
+      },
+    },
+  },
+);
+var User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
 });
+
+function generateUrlTitle(title) {
+  if (title) {
+    // Remueve todos los caracteres no-alfanuméricos
+    // y hace a los espacios guiones bajos.
+    return title.replace(/\s+/g, '_').replace(/\W/g, '');
+  } else {
+    // Generá de forma aleatoria un string de 5 caracteres
+    return Math.random()
+      .toString(36)
+      .substring(2, 7);
+  }
+}
+
 module.exports = {
   Page: Page,
-  User: User
+  User: User,
 };
--- routes/wiki.js
+++ routes/wiki.js
@@ -1,27 +1,40 @@
 const express = require('express');
 const wikiRouter = express.Router();
 var models = require('../models');
-var Page = models.Page; 
-var User = models.User; 
-
-
-
-
+var Page = models.Page;
+var User = models.User;
 
 wikiRouter.get('/', function(req, res, next) {
-    res.redirect('/');
+  res.redirect('/');
 });
 wikiRouter.post('/', function(req, res, next) {
-    res.send('funcionó POST /wiki/');
-    var page = Page.build({
-        title: req.body.title,
-        content: req.body.content
-      });
-    page.save();
+  //   res.send('funcionó POST /wiki/');
+  var page = Page.build({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  page
+    .save()
+    .then(savedPage => {
+      res.redirect(savedPage.route); // route virtual FTW
+    })
+    // .catch(next)
+    .then(console.log(req.body));
 });
 wikiRouter.get('/add', function(req, res, next) {
-    res.render('addpage');
+  res.render('addpage');
+  next();
 });
-
+wikiRouter.get('/:urlTitle', function(req, res, next) {
+  Page.findOne({
+    where: {
+      urlTitle: req.params.urlTitle,
+    },
+  })
+    .then(function(foundPage) {
+      res.render('wikipage', foundPage);
+    })
+    .catch(next);
+});
 
 module.exports = wikiRouter;
